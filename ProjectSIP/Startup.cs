59b921a3b6941734a345ed44895301572e733fc8
Nick <nikitamm59rus@gmail.com>
2019-10-08 23:28:57 +0300

    Add automapper, swagger, postgreSQL options, identity options

--- ProjectSIP/Startup.cs
+++ ProjectSIP/Startup.cs
@@ -1,9 +1,16 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using ProjectSIP.Data;
+using ProjectSIP.Models.Identity;
+using System;
 
 namespace ProjectSIP
 {
@@ -19,12 +26,53 @@ namespace ProjectSIP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            string connection = Configuration.GetConnectionString("PostgreSQL");
+            services.AddEntityFrameworkNpgsql()
+                    .AddDbContext<DatabaseContext>(options =>
+                        options.UseNpgsql(connection));
+
+            // Configure identity
+            services.AddIdentity<User, Role>(identityOptions =>
+            {
+                // configure identity options
+                identityOptions.Password.RequireDigit = false;
+                identityOptions.Password.RequireLowercase = false;
+                identityOptions.Password.RequireUppercase = false;
+                identityOptions.Password.RequireNonAlphanumeric = false;
+                identityOptions.Password.RequiredLength = 6;
+
+                identityOptions.SignIn.RequireConfirmedEmail = false;
+
+                identityOptions.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.@+";
+
+                identityOptions.User.RequireUniqueEmail = true;
+
+                identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(0);
+                identityOptions.Lockout.MaxFailedAccessAttempts = 100;
+            })
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultTokenProviders();
+
+            // auto mapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // swagger for services in ClientApp
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +86,7 @@ namespace ProjectSIP
             {
                 app.UseExceptionHandler("/Error");
             }
+            app.UseSwagger();
 
             app.UseStaticFiles();
             if (!env.IsDevelopment())
@@ -45,13 +94,11 @@ namespace ProjectSIP
                 app.UseSpaStaticFiles();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    template: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
