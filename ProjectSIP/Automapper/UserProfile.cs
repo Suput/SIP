24c9d7810618767b8fa06bb3bb7d4c7b77d170cc
Nick <nikitamm59rus@gmail.com>
2019-10-10 17:06:26 +0300

    Make authentication without jwt

---
+++ ProjectSIP/Automapper/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProjectSIP.Models.Identity;
+using ProjectSIP.Models.Requests.Auth;
+using ProjectSIP.Models.Responses.Identity;
+
+namespace ProjectSIP.Automapper
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, UserView>();
+        }
+    }
+}
