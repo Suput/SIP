dd93db19e37797412805deef266e173db1a4a07d
Nick <nikitamm59rus@gmail.com>
2019-10-17 23:21:07 +0300

    Can get info about user

---
+++ ProjectSIP/Controllers/Account/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjectSIP.Exceptions;
+using ProjectSIP.Models.Identity;
+using ProjectSIP.Models.Responses.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjectSIP.Controllers.Account
+{
+    [Route("api/account")]
+    public class AccountController : MainController
+    {
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(UserManager<User> userManager, ILogger<AccountController> logger) : base (userManager)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult<UserView>> GetUserInfoAsync(int userId)
+        {
+            try
+            {
+                return Ok(await userManager.FindByIdAsync(userId.ToString()));
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex.Message + "\n" + ex.StackTrace);
+                logger.LogError("--- INNER EXCEPTION: ---\n" + ex.InnerException.Message);
+                throw new CantFindUserException();
+            }
+        }
+    }
+}
