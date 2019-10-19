84fcfe3f122edd828f14836286e1f503a04d00b5
Nick <nikitamm59rus@gmail.com>
2019-10-20 01:25:36 +0300

    Add default values to database

---
+++ ProjectSIP/Models/Options/FillDbOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectSIP.Models.Options
+{
+    public class FillDbOptions
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Secondname { get; set; }
+        public string Middlename { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+    }
+}
